683c8bee3bf46011f9256fa204c1fe7647f98c7f
hendy <wanghsinche>
2025-07-06 23:16:09 +0800

    feat: auto cate

--- .eleventy.js
+++ .eleventy.js
@@ -108,6 +108,11 @@ module.exports = function(eleventyConfig) {
 
     return menu;
   });
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/docs/posts/**/*.md").sort(function(a, b) {
+      return b.date - a.date;
+    });
+  });
   
   return {
     dir: {
